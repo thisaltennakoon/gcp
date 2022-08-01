386e1793d2a8066810497f4e3808d2ff01c570eb
Thisal Tennakoon <42693357+thisaltennakoon@users.noreply.github.com>
2022-08-01 23:56:15 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ sudo apt update
 sudo apt install maven -y
 git clone https://github.com/thisaltennakoon/carbon-apimgt
 cd carbon-apimgt
-git checkout 06a5007
+git checkout dc537cd
 mvn clean install
